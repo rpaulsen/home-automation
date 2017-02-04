9d078b9a123ba487ff987f6886b4ebc71166afab
Ryan Paulsen <paulsen.ryan@gmail.com>
2017-02-04 06:05:46 -0500

    Flash lights during Super Bowl.

---
+++ nflupdate.py
@@ -0,0 +1,23 @@
+import nflgame
+from phue import Bridge
+import time
+
+def handleupdate(active, completed, diffs):
+    
+    for diff in diffs:
+        
+        print diff.plays
+        
+        for p in diff.plays:
+            
+            if (p.team == 'ATL' and (p.touchdown == True or p.kicking_fgm>0)):
+                
+                b = Bridge(ip='192.168.1.176',username='hV8sjEd6bLs8Bo9M7UJlVM8sYC4UbHWtO54SN62K')
+                
+                b.set_group(0, {'alert': 'lselect', 'hue': 0, 'sat': 220 })
+
+                time.sleep(5)
+
+                b.set_group(0, {'alert': 'none', 'ct': 443})
+    
+nflgame.live.run(handleupdate)
